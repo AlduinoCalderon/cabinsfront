6a6b0a43c5e97cfdae633a1b16a34346fb751d8b
Alduin <aldocalderon97@gmail.com>
2025-01-08 06:44:13 -0600

    feat: Mail Verification & Front update

--- src/Verified.js
+++ src/Verified.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Container, Button } from './styles/styles';
+import TopBar from './components/TopBar';
 
 const Verified = () => {
   const { email, verification_token } = useParams();
@@ -10,12 +11,13 @@ const Verified = () => {
 
   useEffect(() => {
     if (email && verification_token) {
-        console.log(email, verification_token);
+      console.log(email, verification_token);
       // Hacer una solicitud para verificar el token
       fetch(`https://server-http-mfxe.onrender.com/users/verify-email/${email}/${verification_token}`)
         .then((response) => response.json())
         .then((data) => {
-          if (data.success) {
+          // Verificar si la respuesta contiene el usuario verificado
+          if (data.user) {
             setIsVerified(true);
           } else {
             setErrorMessage('No se pudo verificar el correo. El enlace puede haber expirado.');
@@ -28,9 +30,11 @@ const Verified = () => {
       setErrorMessage('No se ha recibido el token de verificación.');
     }
   }, [email, verification_token]);
+  
 
   return (
     <Container>
+        <TopBar menuItems={[{ label: 'Iniciar Sesión', path: '/' }, { label: 'Registrarse', path: '/signup' }]} />
       <h3>Verificación de Correo</h3>
       {isVerified ? (
         <div>
